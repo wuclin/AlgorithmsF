a296094c6ac19c9f5f980cc4fe81c42dadaafd59
wuclin <845896040@qq.com>
2022-03-14 23:43:50 +0800

    update

--- 1point8/src/NewDateApi.java
+++ 1point8/src/NewDateApi.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 public class NewDateApi {
+
+    public static void main(String[] args){
+        LocalDate localDate ;
+        LocalTime localTime;
+        LocalDateTime localDateTime = LocalDateTime.now();
+        System.out.println(localDateTime);
+    }
 }
